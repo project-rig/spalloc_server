28375624e0b1537edcfb5313f042d86c2c54540c
Jonathan Heathcote <mail@jhnet.co.uk>
2016-02-15 15:37:59 +0000

    Don't test against installed version.
    Something is causing pytest to find the source module and I don't have time to
    track this down.

--- utils/spalloc_server_path.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-"""Print the path of the local spalloc_server installation."""
-
-if __name__=="__main__":  # pragma: no cover
-    import spalloc_server
-    import os.path
-    print(os.path.dirname(spalloc_server.__file__))
