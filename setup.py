67b3669baa68700c6020a2fb5648eb2ccbb3d805
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-04-08 20:26:44 +0100

    Removed inotify dependency

--- setup.py
+++ setup.py
@@ -36,8 +36,7 @@ setup(
 
     # Requirements
     install_requires=[
-        "six", "enum-compat", "inotify_simple", "pytz",
-        "SpiNNMan >= 3.0.0, < 4.0.0"],
+        "six", "enum-compat", "pytz", "SpiNNMan >= 3.0.0, < 4.0.0"],
 
     # Scripts
     entry_points={
