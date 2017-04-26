0432aeeb291001e2774792a1cadff727044d7b94
Christian <brenninc@cs.man.ac.uk>
2017-04-26 09:31:30 +0100

    pep8

--- tests/test_import_all.py
+++ tests/test_import_all.py
@@ -4,5 +4,6 @@ import spinn_utilities.package_loader as package_loader
 def test_import_all():
     package_loader.load_module("spalloc_server")
 
+
 if __name__ == '__main__':
     test_import_all()
