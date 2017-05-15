888df37212aac2735e9c522da766e433182d4666
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-05-15 20:23:21 +0100

    Update version of SpiNNMan

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # Requirements
     install_requires=[
         "six", "enum-compat", "inotify_simple", "pytz",
-        "SpiNNMan >= 3.0.0, < 4.0.0"],
+        "SpiNNMan >=1!4.0.0a1, <1!5.0.0"],
 
     # Scripts
     entry_points={
