4a74dd5325de4f6581e60168b4bcccbf55cb2024
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2018-02-05 10:27:23 +0000

    Try fixing the port to be 17893

--- spalloc_server/async_bmp_controller.py
+++ spalloc_server/async_bmp_controller.py
@@ -8,6 +8,7 @@ from collections import namedtuple, deque
 
 from spinnman.transceiver import create_transceiver_from_hostname
 from spinnman.model import BMPConnectionData
+from spinnman.constants import SCP_SCAMP_PORT
 
 from .links import Links
 
@@ -51,7 +52,7 @@ class AsyncBMPController(object):
 
         self._transceiver = create_transceiver_from_hostname(
             None, 5, bmp_connection_data=[
-                BMPConnectionData(0, 0, hostname, [0], None)])
+                BMPConnectionData(0, 0, hostname, [0], SCP_SCAMP_PORT)])
         self._hostname = hostname
 
         self._stop = False
