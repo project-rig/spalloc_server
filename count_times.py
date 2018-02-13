4dcfb299a361382503ed0cfa88b0b1e0a54d0f86
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2018-02-13 15:46:53 +0000

    Fixed style, and added user count

--- count_times.py
+++ count_times.py
@@ -4,13 +4,16 @@ import time
 
 from collections import defaultdict
 
+
 def get_time(time_str):
     return time.mktime(time.strptime(time_str, '%Y-%m-%d %H:%M:%S,%f'))
-    
+
+
 def get_time_in_hours(seconds):
     m, s = divmod(round(seconds), 60)
     h, m = divmod(m, 60)
     return "{}:{}:{}".format(int(h), int(m), int(s))
+
 
 create_job = defaultdict(list)
 power_on = dict()
@@ -22,21 +25,27 @@ total_machine_test_jobs = 0
 total_machine_test_time = 0
 total_other_jobs = 0
 total_other_time = 0
+other_users = set()
 
 for filename in sys.argv[1:]:
-     with open(filename, "r") as log:
+    with open(filename, "r") as log:
         for line in log:
-            match_create = re.search("(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}): create_job\(\(.*\),(\{.*\})\) from (.*)", line)
-            
+            match_create = re.search(
+                "(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}):"
+                " create_job\(\(.*\),(\{.*\})\) from (.*)", line)
+
             if match_create is not None:
                 date = match_create.group(1)
                 dict_string = match_create.group(2)
                 ip_address = match_create.group(3)
                 dict_object = eval(dict_string)
-                create_job[ip_address].append((dict_object["owner"], get_time(date)))
+                create_job[ip_address].append(
+                    (dict_object["owner"], get_time(date)))
                 continue
-            
-            match_power_on = re.search("(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}): power_job\((\d+),(On|Off)\) from (.*)", line)
+
+            match_power_on = re.search(
+                "(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}):"
+                " power_job\((\d+),(On|Off)\) from (.*)", line)
             if match_power_on is not None:
                 date = match_power_on.group(1)
                 job_id = match_power_on.group(2)
@@ -48,8 +57,10 @@ for filename in sys.argv[1:]:
                         del create_job[ip_address]
                     power_on[job_id] = (owner, start_time)
                 continue
-            
-            match_completed = re.search("(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}): completed shutdown of job (\d+)", line)
+
+            match_completed = re.search(
+                "(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}):"
+                " completed shutdown of job (\d+)", line)
             if match_completed is not None:
                 date = match_completed.group(1)
                 job_id = match_completed.group(2)
@@ -69,16 +80,25 @@ for filename in sys.argv[1:]:
                     else:
                         total_other_time += time_in_seconds
                         total_other_jobs += 1
+                        other_users.add(owner)
                 continue
-            
-            match_destroyed = re.search("(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}): destroy_job", line)
+
+            match_destroyed = re.search(
+                "(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}):"
+                " destroy_job", line)
             if match_destroyed is None:
                 print "Unknown line format for line {}".format(line)
-                        
+
 print "Missing power on for {} jobs".format(len(create_job))
 print "Missing shutdown for {} jobs".format(len(power_on))
 
-print "Total of {} jobs took {}".format(total_jobs, get_time_in_hours(total_time))
-print "Total of {} NMPI jobs took {}".format(total_NMPI_jobs, get_time_in_hours(total_NMPI_time))
-print "Total of {} Machine test jobs took {}".format(total_machine_test_jobs, get_time_in_hours(total_machine_test_time))
-print "Total of {} Other jobs took {}".format(total_other_jobs, get_time_in_hours(total_other_time))
+print "Total of {} jobs took {}".format(
+    total_jobs, get_time_in_hours(total_time))
+print "Total of {} NMPI jobs took {}".format(
+    total_NMPI_jobs, get_time_in_hours(total_NMPI_time))
+print "Total of {} Machine test jobs took {}".format(
+    total_machine_test_jobs, get_time_in_hours(total_machine_test_time))
+print "Total of {} Other jobs took {}".format(
+    total_other_jobs, get_time_in_hours(total_other_time))
+
+print "Total of {} Other users".format(len(other_users))
