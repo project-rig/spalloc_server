d81d34ae1c7453001868f6098af928ae6b32929f
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2018-05-01 11:31:17 +0100

    fixed import all to delete pyc needed is switchig python versions

--- tests/test_import_all.py
+++ tests/test_import_all.py
@@ -1,8 +1,14 @@
+import os
 import spinn_utilities.package_loader as package_loader
 
 
 def test_import_all():
-    package_loader.load_module("spalloc_server")
+    if os.environ.get('CONTINUOUS_INTEGRATION', 'false').lower() == 'true':
+        package_loader.load_module("spalloc_server",
+                                   remove_pyc_files=False)
+    else:
+        package_loader.load_module("spalloc_server",
+                                   remove_pyc_files=True)
 
 
 if __name__ == '__main__':  # pragma: no cover
