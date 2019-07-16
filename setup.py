cb15f61cef64bc8742458b878ea186c4930b7f2c
christian-B <christian.brenninkmeijer@manchester.ac.uk>
2019-07-16 10:27:46 +0100

    added or updated classifiers

--- setup.py
+++ setup.py
@@ -23,12 +23,18 @@ setup(
 
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
 
+        "Natural Language :: English",
+
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS",
 
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     keywords="spinnaker allocation packing management supercomputer",
 
